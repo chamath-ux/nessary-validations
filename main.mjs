1dab6e217989b1cc54e5075258681f5a9a8ca041
chamath1991 <chamath.erplanka@gmail.com>
2024-02-27 22:09:10 +0530

    ninth commit

--- main.mjs
+++ main.mjs
@@ -4,3 +4,9 @@ import { greet, add, PI } from './js/test.mjs';
 console.log(greet('World')); // Output: Hello, World!
 console.log(add(3, 5)); // Output: 8
 console.log(PI); // Output: 3.14159
+
+module.exports =
+{
+    greet,
+    add,
+}
